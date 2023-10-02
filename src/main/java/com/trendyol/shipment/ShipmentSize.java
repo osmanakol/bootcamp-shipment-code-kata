b188fe5a0e45a87ed6e973ac3004d896467e4036
Osman Akol <osmanakol@outlook.com>
2023-10-02 21:46:06 +0300

    implement necasary methods in shipmentsize enum

--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -1,9 +1,31 @@
 package com.trendyol.shipment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum ShipmentSize {
 
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+
+    public ShipmentSize getUpper() {
+        int nextOrdinal= this.ordinal() + 1;
+
+        if (nextOrdinal < ShipmentSize.values().length) {
+            return ShipmentSize.values()[nextOrdinal];
+        }
+
+        return this;
+    }
+
+    public static ShipmentSize getLargestShipmentSize(List<ShipmentSize> sizes) {
+        int maximumOrdinal = sizes.stream()
+                .mapToInt(Enum::ordinal)
+                .max()
+                .orElse(-1);
+
+        return ShipmentSize.values()[maximumOrdinal];
+    }
 }
